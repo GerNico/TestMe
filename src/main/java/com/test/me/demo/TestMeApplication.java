62e741c5f208a28576a86e5cbad13a716fbb4e80
mykola_matsishin@epam.com <mykola_matsishin@epam.com>
2018-01-12 22:08:42 +0200

    Initial commit

---
+++ src/main/java/com/test/me/demo/TestMeApplication.java
@@ -0,0 +1,12 @@
+package com.test.me.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMeApplication.class, args);
+	}
+}
